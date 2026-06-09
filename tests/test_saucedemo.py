1414335173ce5d62443dc45d6c9e0e383304393b
mikerdiaz <mikediazm@gmail.com>
2026-06-08 20:27:07 -0700

    feat: add screenshots to login tests

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -5,11 +5,13 @@ def test_login_success(page):
     login = LoginPage(page)
     login.goto()
     login.login("standard_user", "secret_sauce")
+    page.screenshot(path="screenshots/login_success.png")
     assert "inventory" in page.url
 
 def test_login_failed(page):
     login = LoginPage(page)
     login.goto()
-    login.login("wrong_user" , "wrong_pass")
+    login.login("wrong_user", "wrong_pass")
+    page.screenshot(path="screenshots/login_failed.png")
     error = page.locator("[data-test='error']")
     assert error.is_visible()
